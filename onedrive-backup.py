5b146eab82c01e992445d95b40b18e13a3e59dae
Adam Julier <adamjulier@Adams-MBP.lan1>
2020-02-06 23:34:00 -0500

    one line

--- onedrive-backup.py
+++ onedrive-backup.py
@@ -1,4 +1,2 @@
 import os
-
-os.system("rm -rf /Users/adamjulier/OneDrive/Dev")
-os.system("cp -r /Users/adamjulier/Dev /Users/adamjulier/OneDrive")
+os.system("rm -rf /Users/adamjulier/OneDrive/Dev && cp -r /Users/adamjulier/Dev /Users/adamjulier/OneDrive")
